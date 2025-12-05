a8b76a59e112b066578efd44b6d83992ae1c7f47
Chinmay Shet <chishxd@gmail.com>
2025-12-05 13:59:38 +0530

    day5: part 2

--- day5/day5.py
+++ day5/day5.py
@@ -25,32 +25,36 @@ for start, end in ranges:
     else:
         merged[-1][1] = max(merged[-1][1], end)
 
-spoiled = 0
-fresh = 0
-for id in ids:
-    lo = 0
-    hi = len(merged) - 1
-    ans = -1
+# spoiled = 0
+# fresh = 0
+# for id in ids:
+#     lo = 0
+#     hi = len(merged) - 1
+#     ans = -1
 
-    while lo <= hi:
-        mid = (hi + lo) // 2
-        if merged[mid][0] <= id:
-            lo = mid + 1
-            ans = mid
-        else:
-            hi = mid - 1
+#     while lo <= hi:
+#         mid = (hi + lo) // 2
+#         if merged[mid][0] <= id:
+#             lo = mid + 1
+#             ans = mid
+#         else:
+#             hi = mid - 1
     
-    if ans == -1:
-        # print(f"{id} is spoiled")
-        spoiled +=1
-    else:
-        s, e = merged[ans]
-        if s <= id <= e:
-            # print(f"{id} is fresh")
-            fresh += 1
-        else:
-            # print(f"{id} is spoiled")
-            spoiled += 1
+#     if ans == -1:
+#         # print(f"{id} is spoiled")
+#         spoiled +=1
+#     else:
+#         s, e = merged[ans]
+#         if s <= id <= e:
+#             # print(f"{id} is fresh")
+#             fresh += 1
+#         else:
+#             # print(f"{id} is spoiled")
+#             spoiled += 1
+# print(fresh)
 
-print(fresh)
+total = 0
+for (s, e) in merged:
+    total += (e - s)+ 1
 
+print(total)
